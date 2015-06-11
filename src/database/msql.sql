f82f5444b68b871e5584458e3dbe144d798fd9ea
sindhusha-t <sindhusha55@gmail.com>
2015-06-11 09:01:28 +0530

    Update msql.sql

--- src/database/msql.sql
+++ src/database/msql.sql
@@ -1,9 +1,9 @@
-1) create database stu;
-2) use database stu;
-3) create table students(id integer,name varchar(20));
-4) insert into students values('517','sindhusha');
-5) create table questions(id integer,que varchar(100),opt1 varchar(100),opt2 varchar(100),opt3 varchar(100),opt4 varchar(100),topic varchar(40),ans integer);
-6) insert into questions values('1',' a=5, b=4  then a+b=?','2','5','4','9','Compiler Design','4');
-7) insert into questions values('1','difference between == and === ?','both are type conversion comparison operators','== is strict comparison and === is type converting comparison operators','=== is strict comparison and == is type converting comparison operators','none','Programming Languages','3');
-8) insert into questions values('2','push and pop belongs to?','lists','arrays','stacks','queues','Compiler Design','3');
+create database stu;
+use database stu;
+create table students(id integer,name varchar(20));
+insert into students values('517','sindhusha');
+create table questions(id integer,que varchar(100),opt1 varchar(100),opt2 varchar(100),opt3 varchar(100),opt4 varchar(100),topic varchar(40),ans integer);
+insert into questions values('1',' a=5, b=4  then a+b=?','2','5','4','9','Compiler Design','4');
+insert into questions values('1','difference between == and === ?','both are type conversion comparison operators','== is strict comparison and === is type converting comparison operators','=== is strict comparison and == is type converting comparison operators','none','Programming Languages','3');
+insert into questions values('2','push and pop belongs to?','lists','arrays','stacks','queues','Compiler Design','3');
  
